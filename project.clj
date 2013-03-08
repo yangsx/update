55d3816d3f96311670e6b9c33d62fa2b58fa81b2
Yang Shouxun <yang.shx@fltrp.com>
2013-03-08 18:42:31 +0800

    Use clojure 1.5.0 and tag version 0.1.2.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject update "0.1.1"
+(defproject update "0.1.2"
   :description "A small utility for keeping local git/mercurial repos up-to-date"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main update.core
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [me.raynes/fs "1.4.0"]])
